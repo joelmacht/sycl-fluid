36a3ab583e4bb74ef5bee6956841486f53d00fc6
Joel Macht <joel.macht@student.kit.edu>
2023-11-15 09:37:10 +0100

    wip texture

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <GLFW/glfw3.h>
 
+#include <vector>
+
 int main(void)
 {
     GLFWwindow* window;
@@ -19,11 +21,35 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    int width = 0;
+    int height = 0;
+    glfwGetWindowSize(window, &width, &height);
+
+    std::vector<GLubyte> data;
+    data.resize(width * height * 3);
+
+    for (std::size_t i = 0; i < data.size(); ++i)
+    {
+        // data[i] = static_cast<int>(i);
+        data[i] = static_cast<GLubyte>(i % 256);
+    }
+
+    GLuint tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data.data());
+    glBindTexture(GL_TEXTURE_2D, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glWIndowsp(0, 0);
+        glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, data.data());
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
