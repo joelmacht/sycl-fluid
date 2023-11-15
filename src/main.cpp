457c00b08c09f2a9bb192e6d88b7aabfa922f25d
Joel Macht <macht@zmt.swiss>
2023-11-15 20:15:28 +0100

    Add glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
-#include <glad/gl.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <vector>
 #include <string>
+#include <iostream>
+#include <cassert>
+
+#define GL_CHECK(api_call) { api_call; assert(glGetError() == GL_NO_ERROR);}
 
 int main(void)
 {
@@ -11,6 +15,10 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -22,6 +30,12 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    } 
 
     int width = 0;
     int height = 0;
@@ -37,28 +51,31 @@ int main(void)
     }
 
     GLuint tex;
-    glGenTextures(1, &tex);
-    glBindTexture(GL_TEXTURE_2D, tex);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data.data());
-    glBindTexture(GL_TEXTURE_2D, 0);
+    GL_CHECK(glGenTextures(1, &tex));
+    GL_CHECK(glBindTexture(GL_TEXTURE_2D, tex));
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+    GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+    GL_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data.data()));
+    GL_CHECK(glBindTexture(GL_TEXTURE_2D, 0));
 
-    // GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    // static const GLchar* inline_vertex_shader = "\
-    // #version 460\
-    // \
-    // ";
-    // glShaderSource(vertex_shader, 1, &inline_vertex_shader, nullptr);
-    // glCompileShader(vertex_shader);
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    GL_CHECK({});
+    static const GLchar* inline_vertex_shader = "\
+    #version 460\
+    \
+    void main()\
+    {\
+    }\
+    \
+    ";
+    GL_CHECK(glShaderSource(vertex_shader, 1, &inline_vertex_shader, nullptr));
+    GL_CHECK(glCompileShader(vertex_shader));
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, data.data());
+        GL_CHECK(glClear(GL_COLOR_BUFFER_BIT));
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
